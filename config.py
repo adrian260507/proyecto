99d6100128b7cf92f19a2888e5bf318e8ff1d832
adrian260507 <adriansernagonzalez260507@gmail.com>
2025-11-07 21:21:23 -0500

    Update config.py

--- config.py
+++ config.py
@@ -15,13 +15,13 @@ class Config:
     DB_PORT = int(os.getenv("DB_PORT", os.getenv("MYSQLPORT", "3306")))
     
     # Mail
-    MAIL_SERVER = os.getenv("MAIL_SERVER", "smtp.gmail.com")
-    MAIL_PORT = int(os.getenv("MAIL_PORT", "587"))
+    MAIL_SERVER = os.getenv("MAIL_SERVER", "smtp.sendgrid.net")
+    MAIL_PORT = int(os.getenv("MAIL_PORT", 587))
     MAIL_USE_TLS = True
     MAIL_USE_SSL = False
-    MAIL_USERNAME = os.getenv("MAIL_USERNAME", "asistenciasgtc@gmail.com")
-    MAIL_PASSWORD = os.getenv("MAIL_PASSWORD","")
-    MAIL_DEFAULT_SENDER = os.getenv("MAIL_DEFAULT_SENDER", "Connexa <asistenciasgtc@gmail.com>")
+    MAIL_USERNAME = os.getenv("MAIL_USERNAME", "apikey")
+    MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
+    MAIL_DEFAULT_SENDER = os.getenv("MAIL_DEFAULT_SENDER", "Connexa <notificaciones@asistencia.conexa.com>")
 
     # App
     BASE_URL = os.getenv("BASE_URL", "https://tu-app.railway.app")
@@ -37,4 +37,5 @@ class Config:
     FLASK_HOST = os.getenv("FLASK_HOST", "0.0.0.0")
 
     FLASK_PORT = int(os.getenv("PORT", "5000"))
+
 
