631aab7e83f9130bfeb42047632e6fb1b1f0988e
adrian260507 <adriansernagonzalez260507@gmail.com>
2025-11-10 16:53:10 -0500

    Change mail server configuration to MailerSend
    Updated mail configuration to use MailerSend service.

--- config.py
+++ config.py
@@ -18,15 +18,14 @@ class Config:
     DB_COLLATION = 'utf8mb4_unicode_ci'
     # Timezone
     DB_TIMEZONE = '+00:00'
-    # Mail
-    MAIL_SERVER = os.getenv("MAIL_SERVER", "smtp.sendgrid.net")
-    MAIL_PORT = int(os.getenv("MAIL_PORT", 587))
-    MAIL_USE_TLS = True
-    MAIL_USE_SSL = False
-    MAIL_USERNAME = os.getenv("MAIL_USERNAME", "apikey")
-    MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
-    MAIL_DEFAULT_SENDER = os.getenv("MAIL_DEFAULT_SENDER", "Connexa <notificaciones@asistencia.conexa.com>")
 
+    # Mail - Configuración para MailerSend
+    MAIL_SERVER = os.getenv("MAIL_SERVER", "smtp.mailersend.net")
+    MAIL_PORT = int(os.getenv("MAIL_PORT", "587"))
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.getenv("MAIL_USERNAME", "MS_8U1S8K@test-r6ke4n17nyvgon12.mlsender.net")
+    MAIL_PASSWORD = os.getenv("MAIL_PASSWORD", "mssp.KOreFjK.351ndgw3npr4zqx8.v7CrXDb")
+    MAIL_DEFAULT_SENDER = os.getenv("MAIL_DEFAULT_SENDER", "conexa@test-r6ke4n17nyvgon12.mlsender.net")
     # App
     BASE_URL = os.getenv("BASE_URL", "https://tu-app.railway.app")
     MAIL_TEMPLATE_FOLDER = "mails"
@@ -41,6 +40,7 @@ class Config:
     FLASK_HOST = os.getenv("FLASK_HOST", "0.0.0.0")
 
     FLASK_PORT = int(os.getenv("PORT", "5000"))
+
 
 
 
